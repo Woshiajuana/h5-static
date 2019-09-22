ce63cdf6afc2aa84a9bbc9633bc2bee2da1703f4
Woshiajuana <zhigang.chen@owulia.com>
2019-09-22 18:25:36 +0800

    subcode

---
+++ src/views/main/index.js
@@ -0,0 +1,14 @@
+
+import Toast                        from 'utils/toast.util'
+import Router                       from 'utils/router.util'
+import Store                        from 'utils/store.util'
+
+// 控制器
+const Controller = {
+    timer: null,
+    init () {
+
+    },
+};
+Controller.init();
+
