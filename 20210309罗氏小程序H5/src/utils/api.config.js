8790e7ffb11af00ee352b2ff2043560d0d30e19b
Woshiajuana <zhigang.chen@owulia.com>
2021-03-14 21:58:34 +0800

    subcode

--- 20210309罗氏小程序H5/src/utils/api.config.js
+++ 20210309罗氏小程序H5/src/utils/api.config.js
@@ -2,10 +2,14 @@
 export default {
 
     // 文章详情
-    REQ_ARTICLE_INFO: 'AppletApi/GetBannerInfo',
+    REQ_STUDY_TASK_INFO: 'Home/GetStudyInfo',
 
     // 完成文章
     DO_STUDY_TASK_FINISH: 'Home/CompleteStudy',
+
+    // 收藏
+    DO_COLLECT_ADDED: 'Home/SaveUserCollect',
+
 
 }
 
--- 20210309罗氏小程序H5/src/views/article/index.js
+++ 20210309罗氏小程序H5/src/views/article/index.js
@@ -3,7 +3,6 @@ import './index.scss'
 import 'src/utils/es6-promise.util'
 import Router                       from 'src/utils/router.util'
 import Http                         from 'src/utils/http.util'
-import DateUtil                     from 'src/utils/date.util'
 import storage                      from 'src/utils/storage'
 
 // 控制器
@@ -11,8 +10,14 @@ const Controller = {
     init () {
         this.params = Router.getParams();
         storage.cache.set('$$USER_INFO', this.params);
-        // this.reqArticleInfo();
-        $('.c-button').on('click', this.doStudyTaskFinish.bind(this));
+        this.reqStudyTaskInfo();
+        $('.app').on('click', '.button-sure', this.doStudyTaskFinish.bind(this))
+            .on('click', '.button-collect', this.doCollectAdded.bind(this))
+            .on('click', '.button-return', this.handleReturn.bind(this))
+        ;
+    },
+    handleReturn () {
+        wx.miniProgram.navigateBack();
     },
     doStudyTaskFinish () {
         let { id } = this.params;
@@ -29,21 +34,41 @@ const Controller = {
             });
         }).toast();
     },
-    reqArticleInfo () {
+    doCollectAdded () {
         let { id } = this.params;
+        Http(Http.API.DO_COLLECT_ADDED, {
+            ConfigId: id,
+        }).then(res => {
+            throw '收藏成功';
+        }).toast();
+    },
+    reqStudyTaskInfo () {
+        let { id, type } = this.params;
         if (!id) return null;
-        Http(Http.API.REQ_ARTICLE_INFO, {
-            Data: id,
+        Http(Http.API.REQ_STUDY_TASK_INFO, {
+            ConfigId: id,
         }).then((res) => {
-            let { Name, Content, CreatedDate } = res;
+            let { Title, Content, CreatedDate } = res;
             let strHtml = `
              <div class="header">
-                <div class="title">${ Name }</div>
-                <div class="prompt">
-                    <span>${ DateUtil.formatTime(CreatedDate, 'yyyy-MM-dd') }</span>
-                </div>
+                <div class="title">${ Title }</div>
             </div>
-            <div class="content">${Content}</div>`;
+            <div class="content">${Content}</div>
+            `;
+            if (type === 'task') {
+                strHtml += `
+                <div class="c-button-group">
+                    <button class="c-button button-collect">收藏文章</button>
+                    <button class="c-button button-sure">阅读完成</button>
+                </div>
+                `
+            } else {
+                strHtml += `
+                <div class="c-button-group">
+                    <button class="c-button button-return">返回</button>
+                </div>
+                `
+            }
             $('.app').html(strHtml);
         }).toast();
     }
@@ -51,3 +76,7 @@ const Controller = {
 
 Controller.init();
 
+
+// <div class="prompt">
+//     <span>${ DateUtil.formatTime(CreatedDate, 'yyyy-MM-dd') }</span>
+//     </div>
