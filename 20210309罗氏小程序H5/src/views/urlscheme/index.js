a55eda02d735ae1d75837c4ddc74a335d23bbab7
Woshiajuana <zhigang.chen@owulia.com>
2021-04-14 17:56:59 +0800

    subcode

--- 20210309罗氏小程序H5/src/views/urlscheme/index.js
+++ 20210309罗氏小程序H5/src/views/urlscheme/index.js
@@ -3,23 +3,92 @@ import './index.scss'
 import 'src/utils/es6-promise.util'
 import Router                       from 'src/utils/router.util'
 import Http                         from 'src/utils/http.util'
-import storage                      from 'src/utils/storage'
 
 // 控制器
 const Controller = {
     init () {
-        this.params = Router.getParams();
-        storage.cache.set('$$USER_INFO', this.params);
-        this.reqUrlSchemeInfo();
+        $('#public-web-jump-button').on('click', () => {
+            this.openWeapp();
+        });
+        const ua = navigator.userAgent.toLowerCase()
+        // 企业微信
+        const isWXWork = ua.match(/wxwork/i) == 'wxwork'
+        // 微信浏览器
+        const isWeixin = !isWXWork && ua.match(/MicroMessenger/i) == 'micromessenger'
+        let isMobile = false
+        let isDesktop = false
+        if (navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|IEMobile)/i)) {
+            isMobile = true
+        } else {
+            isDesktop = true
+        }
+
+        if (isDesktop) {
+            // 在 pc 上则给提示引导到手机端打开
+            const containerEl = document.getElementById('desktop-web-container')
+            containerEl.classList.remove('hidden')
+            containerEl.classList.add('full', 'desktop-web-container')
+            // 生成当前页面二维码
+            new QRCode('qrcode', {
+                text: document.URL,
+                width: 200,
+                height: 200,
+                colorDark: '#000000',
+                colorLight: '#ffffff',
+                correctLevel: QRCode.CorrectLevel.H
+            });
+        } else if (isWeixin) {
+            // //如果微信浏览器，通过开放标签打开小程序
+            // const containerEl = document.getElementById('wechat-web-container')
+            // containerEl.classList.remove('hidden')
+            // containerEl.classList.add('full', 'wechat-web-container')
+            //
+            // const launchBtn=document.getElementById('launch-btn')
+            // launchBtn.addEventListener('error', function (e) {
+            //     console.log('用户拒绝跳转或跳转异常', e.detail)
+            // })
+            // const config = await getWXConfig()
+            // wx.config({
+            //     ...config,
+            //     jsApiList: ['chooseImage'], // 安卓上必填一个，随机即可
+            //     openTagList: ['wx-open-launch-weapp'], // 填入打开小程序的开放标签名
+            // })
+        } else {
+            // 在非微信的外部手机浏览器使用URLScheme打开小程序
+            const containerEl = document.getElementById('public-web-container')
+            containerEl.classList.remove('hidden')
+            containerEl.classList.add('full', 'public-web-container')
+
+            const buttonEl = document.getElementById('public-web-jump-button')
+            const buttonLoadingIcon = document.getElementById('public-web-jump-button-loading-icon')
+            const buttonLoadingText = document.getElementById('public-web-jump-button-loading-text')
+            try {
+                // 自动执行，打开URLScheme
+                this.openWeapp(() => {
+                    buttonEl.classList.remove('weui-btn_loading')
+                    buttonLoadingIcon.classList.add('hidden')
+                    buttonLoadingText.classList.add('hidden')
+                })
+            } catch (e) {}
+        }
     },
-    reqUrlSchemeInfo () {
-        let { id } = this.params;
+
+    openWeapp (onBeforeJump) {
+        let { id } = Router.getParams();
         Http(Http.API.REQ_URL_SCHEME_INFO, {
-            Id: +id,
+            Id: id,
         }).then(res => {
-            // window.location.href = res.OpenLink;
+            if (onBeforeJump) {
+                onBeforeJump()
+            }
+            window.location.href = res.OpenLink;
         }).toast();
     },
 };
+
+
+function openWeapp () {
+
+}
 
 Controller.init();
