1082c5a4d8ba9eee85fac0966d6462018c0eeffd
Woshiajuana <zhigang.chen@owulia.com>
2021-05-01 21:04:40 +0800

    subcode

--- 20210323摇一摇/src/views/wx/index.js
+++ 20210323摇一摇/src/views/wx/index.js
@@ -3,7 +3,10 @@ import './index.scss'
 
 $(() => {
     $('.button').on('click', () => {
-        wx.miniProgram.postMessage({ data: { appId: 'wxc1b469219b8df8dc' } });
+        wx.miniProgram.postMessage({ data: {
+            appId: 'wxc1b469219b8df8dc',
+            // path: '', // path 如果不给默认进小程序的首页
+        }});
         wx.miniProgram.navigateBack()
     });
 
